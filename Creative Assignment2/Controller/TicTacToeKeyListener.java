6ab5f4d48aed1cf0139048daed2d80634089bd9b
Blake Brooks <bbrooks13@uco.edu>
2021-02-18 03:02:44 -0600

    Commit

---
+++ Creative Assignment2/Controller/TicTacToeKeyListener.java
@@ -0,0 +1,10 @@
+package Controller;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseListener;
+
+import View.TicTacToePanel;
+
+public class TicTacToeKeyListener implements ActionListener, MouseListener{
+    private TicTacToePanel panel;
+    private int clicks = 0; 
+}
---
+++ Creative Assignment2/View/TicTacToeCanvas.java
@@ -0,0 +1,10 @@
+package View;
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+public class TicTacToeCanvas extends JPanel{
+    public static int width = 400;
+    public static int height = 400;
+    
+}
