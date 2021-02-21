b92d0044d4ca1a734bed06862d7080ae8c5a232c
Blake Brooks <bbrooks13@uco.edu>
2021-02-21 04:30:40 -0600

    Commit

---
+++ CreativeWork2/View/RectangleDrawingCanvas.java
@@ -0,0 +1,12 @@
+package View;
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.util.ArrayList;
+
+
+public class RectangleDrawingCanvas {
+    
+}
