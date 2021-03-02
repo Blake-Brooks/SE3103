df13fccdd661392fc468c5b13714b9b438d8fc03
Blake Brooks <bbrooks13@uco.edu>
2021-03-02 16:00:18 -0600

    commit

---
+++ Lesson3/test/QuestionTest.java
@@ -0,0 +1,14 @@
+package test;
+import model.QuestionBank;
+import java.util.ArrayList;
+import model.Question;
+
+public class QuestionTest {
+    public static void main(String[] args){
+    ArrayList<Question> db = QuestionBank.database;
+    Question q1 = db.get(0);
+    assert q1.checkAnswer("Bill Gates") == false; 
+    assert q1.checkAnswer("James Gosling") == true; 
+    assert q1.checkAnswer("james gosling") == true; 
+    }
+}
