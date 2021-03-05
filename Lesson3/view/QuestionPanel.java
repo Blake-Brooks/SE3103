50d032237f72fd21e270bab54ca46ecd64523a74
Blake Brooks <bbrooks13@uco.edu>
2021-03-05 01:56:47 -0600

    commit

--- Lesson3/view/QuestionPanel.java
+++ Lesson3/view/QuestionPanel.java
@@ -1,15 +1,17 @@
 package view;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import java.awt.GridLayout;
+import java.awt.BorderLayout;
 import java.awt.Container;
 
 public class QuestionPanel {
     private JFrame window;
     private JButton nextButton = new JButton("Next");
-    private JButton previousButton = new JButton("Prev");
+    private JButton prevButton = new JButton("Previous");
     private JButton quitButton = new JButton("Quit");
     private JButton enterButton = new JButton("Enter");
     private JTextField answerField = new JTextField(20);
@@ -21,5 +23,16 @@ public class QuestionPanel {
         Container cp = window.getContentPane(); 
         JPanel southPanel = new JPanel();
         southPanel.setLayout(new GridLayout(2,1));
+        cp.add(BorderLayout.SOUTH, southPanel);
+        JPanel south1 = new JPanel();
+        south1.add(new JLabel("Answer: "));
+        south1.add(answerField);
+        south1.add(enterButton);
+        southPanel.add(south1);
+        JPanel south2 = new JPanel();
+        south2.add(prevButton);
+        south2.add(nextButton);
+        south2.add(quitButton);
+        southPanel.add(south2);
     }
 }
