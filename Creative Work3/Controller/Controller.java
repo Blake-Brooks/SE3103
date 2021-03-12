1bd5ee326ef6a138b9942b99a05767339ae9c16b
Blake Brooks <bbrooks13@uco.edu>
2021-03-12 15:00:38 -0600

    Commit

--- Creative Work3/Controller/Controller.java
+++ Creative Work3/Controller/Controller.java
@@ -1,9 +1,21 @@
 package Controller;
 import Model.Spaceship;
+import View.Game;
+import View.GamePanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Controller {
+import javax.swing.JButton;
+
+public class Controller implements ActionListener{
     private Spaceship spaceship; 
-    public Controller(){
-        
+    private GamePanel gammingPanel;
+    public Controller(GamePanel gammingPanel){
+        this.gammingPanel = gammingPanel;
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JButton button = (JButton) e.getSource();
+        button = gammingPanel.getFireButton();
     }
 }
