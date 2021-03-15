655eb2c119cbed1a9175a6946268875ae8579310
Blake Brooks <bbrooks13@uco.edu>
2021-03-15 02:32:07 -0500

    commit

--- Creative Work3/Controller/Controller.java
+++ Creative Work3/Controller/Controller.java
@@ -4,8 +4,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 
-import Model.Rectangle;
-
 public class Controller implements ActionListener{
     private GamePanel gammingPanel;
     public Controller(GamePanel gammingPanel){
