bc0dd7d2b82a5a7d85342bed8a41550e96831ad4
Blake Brooks <bbrooks13@uco.edu>
2021-03-15 22:40:57 -0500

    commit

---
+++ Creative Work3/View/ImageMove.java
@@ -0,0 +1,12 @@
+package View;
+import java.awt.image.BufferStrategy; 
+
+public class ImageMove implements Runnable {
+    BufferStrategy bufferStrategy; 
+    
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        
+    }
+}
