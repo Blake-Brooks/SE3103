7144bd027fbac8421a317700e4d24acc57b48550
Blake Brooks <bbrooks13@uco.edu>
2021-03-26 14:23:18 -0500

    commit

--- Creative4/view/MovingShapeCanvas.java
+++ Creative4/view/MovingShapeCanvas.java
@@ -2,15 +2,22 @@ package view;
 import java.awt.Color;
 import java.awt.Dimension;
 import javax.swing.JPanel;
+import java.util.ArrayList;
+import java.awt.Color;
+import model.Circle;
+import model.Shape;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 public class MovingShapeCanvas extends JPanel {
     private ShapeMovingPanel panel;
+    private ArrayList<Shape> shapeToBeMoved = new ArrayList<>();
 
     public MovingShapeCanvas(ShapeMovingPanel panel){
         this.panel = panel;
         setBackground(Color.black);
         setPreferredSize(new Dimension(500, 500));
+        shapeToBeMoved.add(new Circle(100, 100, Color.red, 50, false));
     }
 
     @Override
