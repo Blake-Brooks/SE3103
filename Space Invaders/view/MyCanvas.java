a91fa25374270e6bb87c5efbb65e228da2039152
Blake Brooks <bbrooks13@uco.edu>
2021-04-02 00:34:22 -0500

    commit

--- Space Invaders/view/MyCanvas.java
+++ Space Invaders/view/MyCanvas.java
@@ -3,10 +3,15 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.util.ArrayList;
+
 import javax.swing.JPanel;
+
+import model.GameElement;
 
 public class MyCanvas extends JPanel {
     private GameBoard gameBoard;
+    private ArrayList<GameElement> gameElements = new ArrayList<>();
     public MyCanvas(GameBoard gameBoard, int width, int height){
         this.gameBoard = gameBoard;
         setBackground(Color.black);
@@ -17,6 +22,9 @@ public class MyCanvas extends JPanel {
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
+        for( var e: gameElements){
+            e.render(g2);
+        }
     }
     
 }
